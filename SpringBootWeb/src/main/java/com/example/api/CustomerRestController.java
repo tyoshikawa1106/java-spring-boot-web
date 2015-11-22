21421118eb3f538ce7957251115d555d95bc756e
Taiki <tyoshikawa1106@gmail.com>
2015-11-22 20:34:33 +0900

    commit

---
+++ SpringBootWeb/src/main/java/com/example/api/CustomerRestController.java
@@ -0,0 +1,30 @@
+package com.example.api;
+
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/customers")
+public class CustomerRestController {
+    @Autowired
+    CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    List<Customer> getCustomers() {
+        List<Customer> customers = customerService.findAll();
+        return customers;
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    Customer getCustomer(@PathVariable Integer id) {
+        Customer customer = customerService.findOne(id);
+        return customer;
+    }
+}
